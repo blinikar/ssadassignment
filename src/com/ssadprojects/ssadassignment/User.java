f55f2b5377294a261b02dc328a8f90b1930dc43f
fatm1nd <semeritskiy@yandex.ru>
2022-04-22 22:19:32 +0300

    Implement User class Remove User.getPassword() Add User.validatePassword() Remove User.setUsername. It's for doing username instance.
    Add UML1.1 with changes in User class

--- src/com/ssadprojects/ssadassignment/User.java
+++ src/com/ssadprojects/ssadassignment/User.java
@@ -1,4 +1,61 @@
 package com.ssadprojects.ssadassignment;
 
+import java.util.ArrayList;
+
 public class User {
+
+    final private String username;
+
+    private String password;
+
+    private String photoURL = "";
+
+    private ArrayList<Post> posts = new ArrayList<>();
+
+    private String status = "";
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(ArrayList<Post> posts) {
+        this.posts = posts;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void addPost(Post post){
+        this.posts.add(post);
+    }
+
+    public boolean validatePassword(String password){
+        return this.password.equals(password);
+    }
 }
