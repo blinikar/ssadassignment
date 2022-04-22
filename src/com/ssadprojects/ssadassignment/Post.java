d1b8318a96525403e26256d24b3fe938d063d298
fatm1nd <semeritskiy@yandex.ru>
2022-04-22 23:37:00 +0300

    Remove subject -> setSubject and getSubject Set comment getter

--- src/com/ssadprojects/ssadassignment/Post.java
+++ src/com/ssadprojects/ssadassignment/Post.java
@@ -1,4 +1,42 @@
 package com.ssadprojects.ssadassignment;
 
+import java.util.ArrayList;
+
 public class Post {
+
+    private Double rating = 0.0;
+    private ArrayList<String> comments = new ArrayList<>();
+    private String text;
+
+    public Post(String text) {
+        this.text = text;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public ArrayList<String> getComments() {
+        return comments;
+    }
+
+    public String getCommentByOrderNumber(int index){
+        return comments.get(index);
+    }
+
+    public int getAmountOfComments(){
+        return comments.size();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
