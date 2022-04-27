ea90b22654cefffaba1487a303c73896d57d36bc
fatm1nd <semeritskiy@yandex.ru>
2022-04-27 16:49:16 +0300

    Modify comments in User Class

--- src/com/ssadprojects/ssadassignment/User.java
+++ src/com/ssadprojects/ssadassignment/User.java
@@ -6,7 +6,8 @@ import java.io.Serializable;
  * Class User implements system's user object
  * If contains getters and setters.
  * After creation of User, username can not be changed, because of unique of each User.
- * Each user has username, password and URL of avatar photo.
+ * Each user has username, password and URL of avatar photo and status.
+ * User can be Admin, who can modify database of sentiment based keywords.
  */
 public class User implements Serializable {
 
